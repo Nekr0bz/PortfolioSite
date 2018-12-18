8093b59244dcf47f5931101b216fcb4fdd948168
Nekr0bz <Nekr0bz@yandex.ru>
2018-12-18 15:15:17 +0300

    Add: управление моделью Projects

--- main_app/admin.py
+++ main_app/admin.py
@@ -3,4 +3,17 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Projects
+
+
+class ProjectsAdmin(admin.ModelAdmin):
+    """
+    Управление данными о проектах
+    """
+    list_display = ['title']
+    fields = ('title', 'description', 'img', 'site_link', 'github_link')
+
+
+admin.site.register(Projects, ProjectsAdmin)
+
+
