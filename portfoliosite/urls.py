4d1a9f17d6efba46f392f1f5188b90663b4b49ee
Nekr0bz <Nekr0bz@yandex.ru>
2018-12-18 16:37:32 +0300

    Роутинг медиа файлов

--- portfoliosite/urls.py
+++ portfoliosite/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, static
 from django.contrib import admin
+
+import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns += static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
